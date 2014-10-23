169d96107f1450d2ac1cbbdd848b7689c7039b25
Vivek Dasari <vivster7@gmail.com>
2014-10-23 14:47:14 -0400

    reformatted date code

--- timeline.js
+++ timeline.js
@@ -14,7 +14,7 @@ if (Meteor.isClient) {
     var arrow_width = $('#left-arrow').width();
     Session.set('arrow_width_svg', arrow_width);
 
-    //Init
+    //Select first event, with article content "up"
     Session.set("selected_event", 1);
     Session.set("switch", 'up')
 
@@ -23,7 +23,7 @@ if (Meteor.isClient) {
       Session.set("resize_listener", new Date());
     });
 
-    //Arrow key listener
+    //Arrow key listeners
     $(document).keydown(function (evt) {
       //left
       if (evt.keyCode === 37) {
@@ -53,12 +53,22 @@ if (Meteor.isClient) {
   //Responsive timeline
   Template.timeline.helpers({
     selected_event: function() {
-      var event = Events.findOne({id:Session.get("selected_event")});
-      if (!event) return;
-      var date_array = new Date(event.date).toDateString().split(' ');
-      event.month_day = date_array[1] + ', ' + date_array[2];
-      event.year = date_array[3];
-      return event;
+      return Events.findOne({id:Session.get("selected_event")});
+    },
+
+    selected_date: function() {
+      var selected_event = Events.findOne( {id:Session.get("selected_event")},
+                                           { fields:{date:1} }
+                                         );
+      if (selected_event) {
+        var formatted_date_array = new Date(selected_event.date).toDateString()
+        formatted_date_array = formatted_date_array.split(' ').splice(1,4);
+        
+        return selected_date = {
+          month_day: formatted_date_array.slice(0,2).join(" "),
+          year: formatted_date_array[2]
+        };
+      }
     },
 
     events: function() {
@@ -66,6 +76,8 @@ if (Meteor.isClient) {
     },
 
     switch:function() {
+      $('.content').css('transition', 'height 1s ease 0.2');
+      $('.article-content').scrollTop(0);
       return Session.get('switch')
     }
   });
@@ -109,7 +121,8 @@ if (Meteor.isClient) {
     },
 
     formatted_date: function() {
-      return new Date(this.date).toDateString();
+      var formatted_date_array = new Date(this.date).toDateString().split(' ').splice(1,4);
+      return formatted_date_array.join(" ");
     },
 
     distance_pushed: function() {
@@ -117,7 +130,6 @@ if (Meteor.isClient) {
       var resize_listener = Session.get("resize_listener"); 
 
       //Event dates
-      console.log(event_dates);
       var event_dates = Events.find({}, { fields:{date:1}, sort:{date:1} });
       var first_event_date = event_dates.fetch()[0].date;
       var last_event_date = event_dates.fetch()[event_dates.count() - 1].date;
@@ -142,8 +154,14 @@ if (Meteor.isClient) {
 
   //Responsive backgrounds
   Template.style.helpers({
-    selected_event: function() {
-      return Events.findOne({id:Session.get("selected_event")});
+    background_image: function() {
+
+      //Quickly hide content when switching events
+      $('.content').css('transition', 'none');
+      $('.content').removeClass('down');
+      Session.set("switch", "up");
+
+      return Events.findOne({id:Session.get("selected_event")}, { fields:{image:1} });
     }
   });
  
